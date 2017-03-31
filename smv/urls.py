f19ac477c3cb74849fc601de9281338450c0bba8
coderjoker <anandchhibba1996@gmail.com>
2017-04-01 02:15:01 +0530

    basic slider and nav

--- smv/urls.py
+++ smv/urls.py
@@ -16,6 +16,12 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from django.conf.urls import url, include, static
+from django.contrib import admin
+from django.conf import settings
+from .views import home
+
 urlpatterns = [
+    url(r'^$', home),
     url(r'^admin/', admin.site.urls),
-]
+] + static.static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
